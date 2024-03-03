57bcbb96f46821d7a6ec5393e48b633189a31ab3
derick <134869044+DerickProjetos@users.noreply.github.com>
2024-03-03 14:59:21 -0300

    Update app.js

--- app.js
+++ app.js
@@ -40,6 +40,14 @@ app.get("/evoluation-tecnhlogy", (req, res)=>{
 
     res.sendFile(__dirname + "/views/evoluation-tecnhlogy.html")
 })
+app.get("/robots.txt", (req, res)=>{
+
+    res.sendFile(__dirname + "/robots.txt")
+})
+app.get("/sitemap.xml", (req, res)=>{
+
+    res.sendFile(__dirname + "/sitemap.xml")
+})
 
 io.on('connection', (socket) => {
     socket.on("IsConnect", (msg)=>
